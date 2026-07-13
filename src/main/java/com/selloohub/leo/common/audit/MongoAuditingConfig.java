7b4eba656982ccc29ec9eec9741d25599d37e4b7
hanif <hanif.omar@neurogine.com>
2026-07-13 17:41:30 +0800

    feat(common): add Mongo auditing config to enable @CreatedDate/@LastModifiedDate

---
+++ src/main/java/com/selloohub/leo/common/audit/MongoAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.selloohub.leo.common.audit;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoAuditingConfig {
+}
